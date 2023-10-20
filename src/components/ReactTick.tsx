ae7c0034012b751f0956c5b5ed98fac6ac89e780
casdr-cf <cas@cleverfranke.com>
2023-10-20 11:39:44 +0200

    add container recipe

--- src/components/ReactTick.tsx
+++ src/components/ReactTick.tsx
@@ -1,35 +1,13 @@
 import * as d3 from "d3";
-import { hstack, vstack } from "../../styled-system/patterns";
-import { useState } from "react";
+import { useControls } from "leva";
+import { hstack } from "../../styled-system/patterns";
+import { container } from "../recipes/container";
 
 export function ReactTick() {
-  const [start, setStart] = useState(1);
-  const [stop, setStop] = useState(15);
-  const [count, setCount] = useState(10);
+  const { start, stop, count } = useControls({ start: 1, stop: 15, count: 10 });
 
   return (
-    <div className={vstack()}>
-      <input
-        type="number"
-        value={start}
-        onChange={(e) => setStart(+e.target.value)}
-      />
-      <input
-        type="number"
-        value={stop}
-        onChange={(e) => setStop(+e.target.value)}
-      />
-      <div className={hstack()}>
-        <input
-          type="range"
-          min={1}
-          max={20}
-          step={1}
-          value={count}
-          onChange={(e) => setCount(+e.target.value)}
-        />
-        <span>{count}</span>
-      </div>
+    <div className={container()}>
       <ul className={hstack()}>
         {d3.ticks(start, stop, count).map((t) => (
           <li>{t}</li>
---
+++ src/recipes/container.ts
@@ -0,0 +1,8 @@
+import { cva } from "../../styled-system/css";
+
+export const container = cva({
+  base: {
+    backgroundColor: "gray.100",
+    padding: 10,
+  },
+});
